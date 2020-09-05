f735542433595baa6113315036e23849b1c062d9
Lucas Bulgarelli <lucas.bulgarelli@hotmail.com>
2020-09-05 00:46:41 -0300

    use jekyll personal-website theme

--- js/index.js
+++
@@ -1,7 +0,0 @@
-nav_height = $(".navbar").outerHeight();
-me_height = $("#photo-me").outerHeight();
-
-$(window).scroll(function(e) {
-    $(".navbar").css("height", nav_height - $(window).scrollTop());
-    $("#photo-me").css("height", me_height - $(window).scrollTop());
-});
